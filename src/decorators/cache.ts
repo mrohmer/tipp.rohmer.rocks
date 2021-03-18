33b81a28cac9d482502baeacfe9ff2c5b8dd1792
mrohmer <matthias.rohmer@immowelt.de>
2021-03-18 12:18:43 +0100

    patch(cache): add cache decorator

---
+++ src/decorators/cache.ts
@@ -0,0 +1,13 @@
+import {CacheService} from '../services/cache.service';
+import objectHash from 'object-hash';
+
+const buildCacheKey = (prefix: string, ...args: any[]) => `${prefix}--${objectHash(args)}`;
+
+export const Cached = (prefix: string, config: { ttl?: number }) => <T>(target: object, propertyKey: string, descriptor: TypedPropertyDescriptor<(...args) => Promise<T>>) => {
+  const cacheService = new CacheService(config.ttl ?? 60 * 1000);
+  const originalMethod = descriptor.value;
+
+  descriptor.value = (...args): Promise<T> => cacheService.get(buildCacheKey(prefix, ...args), (): Promise<T> => originalMethod(...args))
+
+  return descriptor;
+}
