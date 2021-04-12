6fc2835bbd895abdc94f8be8704c8d78988f658b
mrohmer <matthias.rohmer@immowelt.de>
2021-04-12 19:02:01 +0200

    fix(secrets): move secrets to env vars

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,5 @@
+import {LeagueKey} from "./src/models/kicker/leagues";
+
 module.exports = {
   apps: [
     {
@@ -6,6 +8,16 @@ module.exports = {
       watch: false,
       env: {
         PORT: 8002,
+        TIPP_SESSION_SECRET: '#{SESSION_SECRET}#',
+        TIPP_SESSION_PATH: '#{SESSION_PATH}#',
+        TIPP_MONGO_DB_CONNECTION_STR: '#{MONGO_DB_CONNECTION_STR}#',
+        TIPP_AUTH_DOMAIN: '#{AUTH_DOMAIN}#',
+        TIPP_AUTH_ID: '#{AUTH_ID}#',
+        TIPP_AUTH_SECRET: '#{AUTH_SECRET}#',
+        TIPP_AUTH_CALLBACK_URL: '#{AUTH_CALLBACK_URL}#',
+        TIPP_AUTH_LOGOUT_REDIRECT_URL: '#{AUTH_LOGOUT_REDIRECT_URL}#',
+        TIPP_PUSH_PRIVATE: '#{PUSH_PRIVATE}#',
+        TIPP_PUSH_MAIL: '#{PUSH_MAIL}#',
       },
     },
   ],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,25 +4,25 @@ import {LeagueKey} from '../models/kicker/leagues';
 export const environment: Environment = {
   production: true,
   session: {
-    secret: '#{SESSION_SECRET}#',
-    path: '#{SESSION_PATH}#',
+    secret: process.env.TIPP_SESSION_SECRET,
+    path: process.env.TIPP_SESSION_PATH,
   },
   db: {
-    URI: '#{MONGO_DB_CONNECTION_STR}#',
+    URI: process.env.TIPP_MONGO_DB_CONNECTION_STR,
   },
   auth: {
-    domain: '#{AUTH_DOMAIN}#',
-    clientID: '#{AUTH_ID}#',
-    clientSecret: '#{AUTH_SECRET}#',
-    callbackURL: '#{AUTH_CALLBACK_URL}#',
-    logoutRedirect: '#{AUTH_LOGOUT_REDIRECT_URL}#',
+    domain: process.env.TIPP_AUTH_DOMAIN,
+    clientID: process.env.TIPP_AUTH_ID,
+    clientSecret: process.env.TIPP_AUTH_SECRET,
+    callbackURL: process.env.TIPP_AUTH_CALLBACK_URL,
+    logoutRedirect: process.env.TIPP_AUTH_LOGOUT_REDIRECT_URL,
   },
   leagues: [
     LeagueKey.EM_U21,
   ],
   push: {
     publicKey: '#{PUSH_PUBLIC}#',
-    privateKey: '#{PUSH_PRIVATE}#',
-    mail: '#{PUSH_MAIL}#',
+    privateKey: process.env.TIPP_PUSH_PRIVATE,
+    mail: process.env.TIPP_PUSH_MAIL,
   },
 };
