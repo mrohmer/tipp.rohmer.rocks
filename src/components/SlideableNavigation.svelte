826994cf83fc4df15993570bca95a1e5d3de4f4d
mrohmer <matthias.rohmer@immowelt.de>
2021-04-12 21:10:32 +0200

    patch(navigation): add swipe gesture detection

--- src/components/SlideableNavigation.svelte
+++ src/components/SlideableNavigation.svelte
@@ -1,36 +1,69 @@
 <script lang="ts">
   import Icon from "./Icon.svelte";
-    export let linkLeft: string;
-    export let tooltipLeft: string;
-    export let tooltipRight: string;
-    export let linkRight: string;
-    export let hasHeader = true;
+  import { goto } from "@sapper/app";
+
+  const swipe = async (node: HTMLElement) => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+    const events: Array<string> = [
+      'swipe',
+      'swipeleft',
+      'swiperight',
+      'swipeup',
+      'swipedown'
+    ];
+    const Hammer = await import('hammerjs');
+
+    const hammer = new Hammer.default(node);
+    for (const event of events) {
+      hammer.on(event, (ev: any) => node.dispatchEvent(new CustomEvent(event, { detail: ev })));
+    }
+  }
+
+  export let linkLeft: string;
+  export let tooltipLeft: string;
+  export let tooltipRight: string;
+  export let linkRight: string;
+  export let hasHeader = true;
+
+  function swipeLeft() {
+    if (linkRight) {
+      goto(linkRight, {});
+    }
+  }
+
+  function swipeRight() {
+    if (linkLeft) {
+      goto(linkLeft, {});
+    }
+  }
 </script>
 
 <style type="text/scss">
-    .header {
-      display: flex;
+  .header {
+    display: flex;
 
-      &__link {
-        width: 50px;
-        text-align: center;
+    &__link {
+      width: 50px;
+      text-align: center;
 
-        a {
-          display: block;
-          width: 100%;
-          padding: 5px;
-          box-sizing: border-box;
-        }
-      }
-
-      &__title {
-        flex-grow: 1;
-        text-align: center;
+      a {
+        display: block;
+        width: 100%;
+        padding: 5px;
+        box-sizing: border-box;
       }
     }
+
+    &__title {
+      flex-grow: 1;
+      text-align: center;
+    }
+  }
 </style>
 {#if hasHeader}
-    <div class="header">
+    <div class="header" use:swipe on:swipeleft={swipeLeft} on:swiperight={swipeRight}>
         <div class="header__link">
             {#if linkLeft}
                 <a href="{linkLeft}" title="{tooltipLeft}">
@@ -44,11 +77,12 @@
         <div class="header__link">
             {#if linkRight}
                 <a href="{linkRight}" title="{tooltipRight}">
-                    <Icon>arrow-right</Icon></a>
+                    <Icon>arrow-right</Icon>
+                </a>
             {/if}
         </div>
     </div>
 {/if}
-<div class="content">
-    <slot />
+<div class="content" use:swipe on:swipeleft={swipeLeft} on:swiperight={swipeRight}>
+    <slot/>
 </div>
