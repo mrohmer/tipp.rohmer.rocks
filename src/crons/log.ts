c5d3024bc8777a413e4ae6fc95af2fd111e4c231
mrohmer <rohmer.matze@gmail.com>
2021-06-27 11:00:24 +0200

    chore(log): add time to logs

--- src/crons/log.ts
+++ src/crons/log.ts
@@ -1,2 +1,19 @@
-export const log = (name: string, ...messages) => console.log('[cron]', name, ...messages);
-export const error = (name: string, ...messages) => console.error('[cron]', name, ...messages);
+export const log = (name: string, ...messages) => console.log(time(), '[cron]', name, ...messages);
+export const error = (name: string, ...messages) => console.error(time(), '[cron]', name, ...messages);
+
+const time = () => {
+  const date = new Date();
+
+  return [
+    [
+      date.getFullYear(),
+      String(date.getMonth() + 1).padStart(2, '0'),
+      String(date.getDate()).padStart(2, '0'),
+    ].join('-'),
+    [
+      String(date.getHours()).padStart(2, '0'),
+      String(date.getMinutes()).padStart(2, '0'),
+      String(date.getSeconds()).padStart(2, '0'),
+    ].join(':')
+  ].join(' ')
+}
